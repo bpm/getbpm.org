0697a8260dd4dc1bdbe74a1cadc2993d2fb48007
Peter Wagenet <peter.wagenet@gmail.com>
2011-08-18 10:06:32 -0700

    Removed deploy code

--- config/deploy/production.rb
+++
@@ -1,6 +0,0 @@
-role :app, "rubycentral.org"
-role :web, "rubycentral.org"
-role :db,  "rubycentral.org", :primary => true
-
-set :branch,    "production"
-set :deploy_to, "/var/www/rubycentral/gemcutter.org/"
